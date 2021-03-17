e4301bda4a4fad620709f6b6b8bbb3ecdbb7ffe4
= <saleh.majeed8@gmail.com>
2021-03-17 10:05:33 +0530

    updated

--- test.js
+++ test.js
@@ -25,33 +25,60 @@ const student = new mongoose.Schema(
 				type: Number,
 			},
 		},
+		school: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'school',
+		},
 	},
 	{
 		timestamps: true,
 	}
 );
 
+const school = new mongoose.Schema({
+	name: String,
+});
+
 // Create Model
 const Student_model = mongoose.model('student', student);
+const School = mongoose.model('school', school);
 
 connect()
 	.then(async connection => {
-		// Fill Value
-		const student = await Student_model.create({
-			firstName: 'tim',
-		});
-
-		const found = await Student_model.find({
-			firstName: 'thi',
-		});
-
-		const found_by_id = await Student_model.findById('2131213j');
-
-		const updated = await Student_model.findByIdAndUpdate('sfd2', {});
-
-		console.log(student);
-		console.log(found);
-		console.log(found_by_id);
-		console.log(updated);
+		// for_student();
+		for_school();
 	})
 	.catch(e => console.error(e));
+
+async function for_school() {
+	const school = await School.create({ name: 'mlk elementry' });
+	const student = await Student_model.create({
+		firstName: 'Trisha',
+		school: school._id,
+	});
+
+	const match = await Student_model.findById(student.id).populate('school').exec();
+
+	console.log(match);
+}
+
+async function for_student() {
+	// Fill Value
+	const student = await Student_model.create({
+		firstName: 'tim',
+	});
+
+	const found = await Student_model.find({
+		firstName: 'thi',
+	});
+
+	const found_by_id = await Student_model.findById('2131213j');
+
+	const updated = await Student_model.findByIdAndUpdate('sfd2', {});
+
+	console.log(student);
+	console.log(found);
+	console.log(found_by_id);
+	console.log(updated);
+}
